dc042ffca6f714cc866f2b1306953c320ce3342e
baccano24 <735646422@qq.com>
2020-10-19 10:30:18 +0800

    搜索

--- pages/center.vue
+++ pages/center.vue
@@ -1,31 +1,38 @@
 <template>
   <div>
-    center
+    搜索：<input type="text" v-model="search" @input="searchList(search)" />
     <ul>
-      <li v-for="data in dataList" :key="data" @click=handleClick(data)>
-        {{data}}
+      <li v-for="data in movieList" :key="data.id">
+        <p>{{data.nm}}</p>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
-      dataList:["1111","2222","3333"]
-    }
+      search: "",
+      movieList: [],
+    };
   },
   methods: {
-    handleClick(id){
-      this.$router.push(`/detail/${id}`)
-    }
+    searchList(data) {
+      // console.log(this.search);
+      this.search = data;
+      axios(`/ajax/search?kw=${this.search}&cityId=1&stype=-1`).then((res) => {
+        // console.log(res.data.movies);
+        this.movieList = res.data.movies.list;
+      }).catch(err=>{
+        console.log(err)
+      })
+    },
   },
-}
+};
 </script>
 
 <style scoped>
-ul{
 
-}
 </style>
